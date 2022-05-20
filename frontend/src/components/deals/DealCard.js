707dface73f0b8fa69be66946a4b86f6e68e70c6
dmsifu <dmsifu@gmail.com>
2022-05-19 22:33:09 -0500

    made deal grid responsive

--- frontend/src/components/deals/DealCard.js
+++ frontend/src/components/deals/DealCard.js
@@ -3,10 +3,16 @@ function DealCard({ title, offerPrice, originalPrice, productLink, productImageL
   return (
     <div className="deal-card">
       <a href={productLink}>
-        <img src={productImageLink} alt={`Image for ${title}`} />
-        <h1 className="title">{title}</h1>
-        <h1 className="offer-price">{offerPrice}</h1>
-        <h1 className="original-price">{originalPrice}</h1>
+        <div className="deal-image">
+          <img src={productImageLink} alt={`Image for ${title}`} />
+        </div>
+        <div className="title-container">
+          <h1 className="title">{title}</h1>
+        </div>
+        <div className="price-container">
+          <h1 className="offer-price">{offerPrice}</h1>
+          <h1 className="original-price">{originalPrice}</h1>
+        </div>
       </a>
     </div>
   )
