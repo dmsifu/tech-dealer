b7befe7bb21bc8dc330074d3fc55daed9e90b083
dmsifu <dmsifu@gmail.com>
2022-06-04 15:12:13 -0500

    prepare deploy 2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const scrapeTodaysData = require('./scheduled jobs/getTodaysData')
 
