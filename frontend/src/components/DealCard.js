1f62a1043aa7bdd7da0c07212befa4dcefa25975
dmsifu <dmsifu@gmail.com>
2022-06-14 22:30:04 -0500

    added animation to percent off box

--- frontend/src/components/DealCard.js
+++ frontend/src/components/DealCard.js
@@ -1,9 +1,25 @@
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
 
 function DealCard({ title, offerPrice, originalPrice, percentOff, productLink, productImageLink, soldOn }) {
 
+  const controls = useAnimation()
+
+  function percentAnimation(test){
+    if(test){
+      controls.start({ 'boxShadow': '0px 5px 10px 0px rgba(38,113,243,0.9)' , y: 3, transition: { delay: .15 }})
+    }
+    else{
+      controls.start({ 'boxShadow': 'none', y: 0 })
+    }
+  }
+
   return (
-    <motion.div className="deal-card" whileHover={{ y: 5 }}>
+    <motion.div 
+      className="deal-card" 
+      whileHover={{ y: 5 }} 
+      onHoverStart={e => percentAnimation(true)} 
+      onHoverEnd={e => percentAnimation(false)}
+    >
       <a href={productLink}>
         <div className="deal-image">
           <img src={productImageLink} alt={title} />
@@ -15,7 +31,7 @@ function DealCard({ title, offerPrice, originalPrice, percentOff, productLink, p
         <div className="price-container">
           <h2 className="offer-price">{offerPrice}</h2>
           <h2 className="original-price">{originalPrice}</h2>
-          <h2 className="percent-off">{`${percentOff} Off`}</h2>
+          <motion.h2 className="percent-off" animate={controls} >{`${percentOff} Off`}</motion.h2>
         </div>
       </a>
     </motion.div>
